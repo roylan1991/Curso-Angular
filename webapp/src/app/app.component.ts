bae486481852dc0e13ecff90ad0ed605b98f2a8b
Roylan Alejandro Bressler Camps <43889144+roylan1991@users.noreply.github.com>
2020-10-09 22:33:54 -0400

    Directivas ngIf, ngClass y ngFor

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webapp'; 
+  title = 'webapp';
+  show: boolean = true;
+  fondo: string = '';
+  cursos: string[] = ['HTML', 'AngularJS', 'ReactJS'];
+  animales: any[] = [
+    { tipo: 'perro', nombre: 'Sasha', edad: 1 },
+    { tipo: 'tortuga', nombre: 'Alicia', edad: 29 },
+    { tipo: 'delfin', nombre: 'Flipper', edad: 12 },
+  ]
 }
